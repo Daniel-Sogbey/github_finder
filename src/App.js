c2114f1b61168bfa02097857e77ce57eb0babeda
Daniel-Sogbey <mathematics06physics@gmail.com>
2021-03-03 12:41:11 -0800

    http request and spinner

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layouts/Navbar";
 import Users from "./components/users/Users";
+import axios from "axios";
 
 class App extends Component {
+	state = {
+		users: [],
+		loading: false,
+	};
+
 	// state = {
 	// id: "id",
 	// login: "mojombo",
@@ -11,12 +17,25 @@ class App extends Component {
 	// html_url: "https://github.com/mojombo",
 	// };
 
+	async componentDidMount() {
+		// console.log(123);
+
+		this.setState({
+			loading: true,
+		});
+
+		const res = await axios.get("https://api.github.com/users");
+		console.log(res.data);
+
+		this.setState({ users: [...res.data], loading: false });
+	}
+
 	render() {
 		return (
 			<div className="App">
 				<Navbar />
 				<div className="container">
-					<Users />
+					<Users users={this.state.users} loading={this.state.loading} />
 				</div>
 			</div>
 		);
