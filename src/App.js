34d4f561881ea95fe9bd79af2c3fc315cafac46e
Daniel-Sogbey <mathematics06physics@gmail.com>
2021-03-03 12:55:01 -0800

    added environment variables

--- src/App.js
+++ src/App.js
@@ -24,7 +24,12 @@ class App extends Component {
 			loading: true,
 		});
 
-		const res = await axios.get("https://api.github.com/users");
+		const res = await axios.get(
+			`https://api.github.com/users?client_id=${process.env
+				.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env
+				.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+		console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
 		console.log(res.data);
 
 		this.setState({ users: [...res.data], loading: false });
