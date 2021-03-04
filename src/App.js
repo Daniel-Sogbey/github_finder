242f4c245b94f8786581857f8770725986b2af1e
Daniel-Sogbey <mathematics06physics@gmail.com>
2021-03-04 15:35:35 -0800

    ....ignore

--- src/App.js
+++ src/App.js
@@ -10,13 +10,6 @@ class App extends Component {
 		loading: false,
 	};
 
-	// state = {
-	// id: "id",
-	// login: "mojombo",
-	// avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-	// html_url: "https://github.com/mojombo",
-	// };
-
 	async componentDidMount() {
 		// console.log(123);
 
