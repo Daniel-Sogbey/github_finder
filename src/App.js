e3862e925355d527f9f4e7de9b58010450a0e665
Daniel-Sogbey <mathematics06physics@gmail.com>
2021-03-06 08:26:07 -0800

    added react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/layouts/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
 import Alert from "./components/layouts/Alert";
+import About from "./components/Pages/About";
 import axios from "axios";
 
 class App extends Component {
@@ -77,20 +79,31 @@ class App extends Component {
 	render() {
 		const { loading, users, alert } = this.state;
 		return (
-			<div className="App">
-				<Navbar />
-				<div className="container">
-					<Alert alert={alert} />
-					<Search
-						searchUsers={this.searchUsersHandler}
-						clearUsers={this.clearUsersHandler}
-						users={users}
-						setAlert={this.setAlertHandler}
-					/>
-
-					<Users users={users} loading={loading} />
+			<BrowserRouter>
+				<div className="App">
+					<Navbar />
+					<div className="container">
+						<Alert alert={alert} />
+						<Switch>
+							<Route
+								exact
+								path="/"
+								render={props =>
+									<Fragment>
+										<Search
+											searchUsers={this.searchUsersHandler}
+											clearUsers={this.clearUsersHandler}
+											users={users}
+											setAlert={this.setAlertHandler}
+										/>
+										<Users users={users} loading={loading} />
+									</Fragment>}
+							/>
+							<Route exact path="/about" component={About} />
+						</Switch>
+					</div>
 				</div>
-			</div>
+			</BrowserRouter>
 		);
 	}
 }
---
+++ src/components/Pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+	return (
+		<div>
+			<h1>Github User Finder App</h1>
+			<p>Find Github users with just a search on a clean user interface</p>
+			<p>Version: 1.0.0 </p>
+		</div>
+	);
+};
+
+export default About;
