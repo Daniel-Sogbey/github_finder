d8d095ddd1438cc97fb92f4d64b13fcb1ceffaff
Daniel-Sogbey <mathematics06physics@gmail.com>
2021-03-07 00:01:04 -0800

    added link to user page

--- src/App.js
+++ src/App.js
@@ -120,7 +120,7 @@ class App extends Component {
 								exact
 								path="/user/:login"
 								render={props =>
-									<User {...props} getUser={this.getUser} user={user} />}
+									<User {...props} getUser={this.getUser} user={user} loading ={loading} />}
 							/>
 						</Switch>
 					</div>
